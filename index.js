ec8b29ad11fd0352049c92ebedca289e4f9aee46
Edwin Meek <emeekiv@gmail.com>
2019-12-19 15:42:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return Object.assign(object, {[key]:value})
 }
 function deleteFromObjectByKey(object, key){
-  delete object.key;
-  return object
+  var newObj = delete object.key;
+  return newObj
 }
