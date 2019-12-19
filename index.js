f641428a9386a75cc99c7965b56b263022bd9d30
Edwin Meek <emeekiv@gmail.com>
2019-12-19 15:40:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ function updateObjectWithKeyAndValue(object, key, value){
   var newObj = Object.assign({}, object, {[key]:value})
   return newObj
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return Object.assign(object, {[key]:value})
+}
